1634ea90cdc5c478a4cfde7ae7ffdfdd01a647f9
Malyssa Winters <malyssawinters5@gmail.com>
2016-06-14 15:56:09 -0400

    commit 3

--- GoWithTheFlow/Program.cs
+++ GoWithTheFlow/Program.cs
@@ -10,6 +10,32 @@ namespace GoWithTheFlow
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Lamp Does Not Work");
+            Console.WriteLine("Is your lamp plugged in?");
+
+            string answer = Console.ReadLine();
+
+            
+                if (answer.ToLower() == "yes")
+                {
+                Console.WriteLine("Is the bulb burned out?");
+                answer = Console.ReadLine();
+
+                
+                    if (answer.ToLower() == "yes")
+                        Console.WriteLine("Replace bulb.");
+                else
+                { Console.WriteLine("Repair your lamp.");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Plug in your lamp");
+            }
+        
+            Console.ReadKey();
+
         }
     }
 }
